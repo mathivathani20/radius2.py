72fab470e1efaa1122d07d43dc5b3a7f036d2a6d
mathivathani20 <88815202+mathivathani20@users.noreply.github.com>
2021-08-12 19:51:50 +0530

    Update Prg.py
    This programme created by me

--- Prg.py
+++ Prg.py
@@ -1,3 +1,9 @@
 from math import pi
 r=float(input("enter radius:"))
 Print("area of circle is"+str(pi*r**2))
+
+
+
+filename=int(input("enter filename"))
+f.externs=filename.split(".")
+Print("the externs of the file is"+f[-1])
